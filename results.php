d36f4f0f1f667062e8c78a12e8d7078d2b692deb
s.ditaranto <simodita99@gmail.com>
2024-03-15 14:07:39 +0100

    stampa a schermo paragrafo e lunghezza

--- results.php
+++ results.php
@@ -1,41 +1,39 @@
+<?php
+
+    $text = $_GET['user-text'];
+
+    $text_explode = explode(' ', $text);
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - prima pagina</title>
+    <title>PHP - risultati</title>
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <!-- style -->
-    <style>
-
-        #user-text {
-            width: 300px;
-        }
-
-    </style>
 
 </head>
 
 <body>
     
-    <div class="container">
+    <h1 class="text-center">BadWords</h1>
 
-        <h1 class="text-center">BadWords</h1>
+    <div class="container border border-primary">
 
-        <form action="results.php" class="d-flex gap-2">
+        <div class="text">
+            <span>Testo inserito:</span>
+            <p>
+                <?php 
+                    echo $text . "<br> Lunghezza testo: " . strlen($text) . " caratteri";
+                ?>
+            </p>
 
-            <div>
-                <label for="">Inserisci un testo</label>
-                <input type="text" id="user-text" name="user-text" placeholder="Inserisci qui">
-            </div>
-
-            <input type="submit">
-        
-        </form>
+        </div>
 
 
     </div>
